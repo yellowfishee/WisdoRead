f9ae1fe9089c793715b723f376cf432c4f680119
yellowfish <hxchen1@163.com>
2023-08-15 03:23:47 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import { invoke } from '@tauri-apps/api'
+
+createApp(App).mount('#app')
+
+invoke('greet', { name: 'World' })
+    .then((res) => {
+        console.log(res)
+    }
+    )
